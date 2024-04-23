b6ac2564f4b3b8b333195e8a7da6cedf9b3a16ad
Antonio-pf <antoniopfelipe03@gmail.com>
2024-04-23 20:17:09 -0300

    renderizando view de

---
+++ app/views/negociacoes-view.ts
@@ -0,0 +1,31 @@
+export class NegociacoesView {
+
+    private elemento: HTMLElement;
+
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+
+    template():string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>Data</th>
+                    <th>Quantidade</th>
+                    <th>Valor</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                
+            </tbody>
+        </table>
+        `;
+    }
+
+    // rederiza o template no elemento do seletor
+    update():void {
+        this.elemento.innerHTML = this.template();
+    }
+}
