eb8c75991ad5ae149a31da6ccfb017259b1f8121
Antonio-pf <antoniopfelipe03@gmail.com>
2024-05-02 22:27:09 -0300

    fix: add strictNullCkecks

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -13,10 +13,11 @@ export class NegociacaoController {
     private mensagemView = new MensagemView('#mensagemView');
     private readonly SABADO = 6;
     private readonly DOMINGO = 0;
+    
     constructor() {
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
+        this.inputData = document.querySelector('#data') as HTMLInputElement;
+        this.inputQuantidade = document.querySelector('#quantidade') as HTMLInputElement;
+        this.inputValor = document.querySelector('#valor') as HTMLInputElement;
         this.negociacoesView.update(this.negociacoes);
     }
 
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -8,7 +8,6 @@ export class Negociacao {
         return this.quantidade * this.valor;
     }
     get data() {
-        // evito que na chamada seja modificado a data com o setDate
         const data = new Date(this._data.getTime());
         return data;
     }
